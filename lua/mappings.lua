be5bee4a8eea42707e242069eb64582b63c01ff9
qwertasyx <qwertasyx@googlemail.com>
2024-07-22 10:05:20 +0200

    add Copilot and lsp mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,14 @@ map("n", "<C-n>", ":Telescope git_files <CR>", { desc = "Telescope Files" })
 map("n", "<C-u>", "<C-u>zz", { desc = "move up" })
 map("n", "<C-d>", "<C-d>zz", { desc = "move down" })
 
+-- lsp stuff
+map("n", "<leader>ls", function()
+  vim.lsp.buf.signature_help()
+end, { desc = "LSP Signature Help" })
+map("n", "<leader>lf", function()
+  vim.diagnostic.open_float { border = "rounded" }
+end, { desc = "LSP Signature Help" })
+
 -- Disable mappings
 local nomap = vim.keymap.del
 
@@ -23,3 +31,14 @@ nomap("n", "<leader>fm")
 map("n", "gf", function()
   require("conform").format { lsp_fallback = true }
 end, { desc = "format files" })
+
+-- Copilot
+map("i", "<C-l>", function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept" }, {
+  replace_keycodes = true,
+  nowait = true,
+  silent = true,
+  expr = true,
+  noremap = true,
+})
